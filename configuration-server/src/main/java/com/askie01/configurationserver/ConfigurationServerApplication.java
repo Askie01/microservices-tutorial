2e2d1c183a9ca3707b4c3d2f1ceb63a6284fb994
Askie01 <adam.paw01@gmail.com>
2025-09-28 13:56:30 +0200

    Clean up `configuration-server` microservice
    * Fixed styling in the main class of microservice.
    * Updated spring boot version in `pom.xml` to 3.5.4

--- configuration-server/src/main/java/com/askie01/configurationserver/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/com/askie01/configurationserver/ConfigurationServerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigurationServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServerApplication.class, args);
     }
-
 }
