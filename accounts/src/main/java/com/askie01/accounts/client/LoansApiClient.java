450384214b203a00bc7c98330e7b266c032b8f38
Askie01 <adam.paw01@gmail.com>
2025-10-22 20:29:27 +0200

    Create `LoansApiClient`
    * Created feign client interface to communicate with `loans` microservice and receive loan-related information.

---
+++ accounts/src/main/java/com/askie01/accounts/client/LoansApiClient.java
@@ -0,0 +1,14 @@
+package com.askie01.accounts.client;
+
+import com.askie01.accounts.dto.LoanDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "loans", path = "/loans")
+public interface LoansApiClient {
+
+    @GetMapping
+    ResponseEntity<LoanDTO> getLoan(@RequestParam String mobileNumber);
+}
