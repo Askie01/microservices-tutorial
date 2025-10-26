fcb6f745523a321356a60fccff12af49161fdc06
Askie01 <adam.paw01@gmail.com>
2025-10-26 17:03:26 +0100

    Update `Askie01RouteLocatorConfiguration`
    * Moved duplicated values to variables and replaced the duplicates with variable references.
    * Added a response header for `X-Response-Time` to know when the request was processed.

--- api-gateway/src/main/java/com/askie01/apigateway/configuration/Askie01RouteLocatorConfiguration.java
+++ api-gateway/src/main/java/com/askie01/apigateway/configuration/Askie01RouteLocatorConfiguration.java
@@ -5,23 +5,34 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+
 @Configuration
 public class Askie01RouteLocatorConfiguration {
 
     @Bean
     public RouteLocator askie01RouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        final String segmentReplacement = "/${segment}";
+        final String responseTimeHeaderName = "X-Response-Time";
+        final String responseTimeHeaderValue = LocalDateTime.now().toString();
         return routeLocatorBuilder.routes()
                 .route(path -> path
                         .path("/askie01/accounts/**")
-                        .filters(filter -> filter.rewritePath("/askie01/accounts/(?<segment>.*)", "/${segment}"))
+                        .filters(filter -> filter
+                                .rewritePath("/askie01/accounts/(?<segment>.*)", segmentReplacement)
+                                .addResponseHeader(responseTimeHeaderName, responseTimeHeaderValue))
                         .uri("lb://accounts"))
                 .route(path -> path
                         .path("askie01/cards/**")
-                        .filters(filter -> filter.rewritePath("/askie01/cards/(?<segment>.*)", "/${segment}"))
+                        .filters(filter -> filter
+                                .rewritePath("/askie01/cards/(?<segment>.*)", segmentReplacement)
+                                .addResponseHeader(responseTimeHeaderName, responseTimeHeaderValue))
                         .uri("lb://cards"))
                 .route(path -> path
                         .path("/askie01/loans/**")
-                        .filters(filter -> filter.rewritePath("/askie01/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(filter -> filter
+                                .rewritePath("/askie01/loans/(?<segment>.*)", segmentReplacement)
+                                .addResponseHeader(responseTimeHeaderName, responseTimeHeaderValue))
                         .uri("lb://loans"))
                 .build();
     }
