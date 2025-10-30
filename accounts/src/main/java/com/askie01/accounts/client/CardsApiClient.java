42596bee878f12e4e1d32c4dfab9399beaac2830
Askie01 <adam.paw01@gmail.com>
2025-10-30 21:10:08 +0100

    Setup default fallback strategy for `CardsApiClient` inside `accounts` microservice
    * Added a fallback strategy inside `CardsApiClient` that points to `CardsApiClientFallback.class` so we have our fallback strategy enabled.

--- accounts/src/main/java/com/askie01/accounts/client/CardsApiClient.java
+++ accounts/src/main/java/com/askie01/accounts/client/CardsApiClient.java
@@ -1,12 +1,13 @@
 package com.askie01.accounts.client;
 
 import com.askie01.accounts.dto.CardDTO;
+import com.askie01.accounts.fallback.CardsApiClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "cards", path = "/cards")
+@FeignClient(name = "cards", path = "/cards", fallback = CardsApiClientFallback.class)
 public interface CardsApiClient {
 
     @GetMapping
