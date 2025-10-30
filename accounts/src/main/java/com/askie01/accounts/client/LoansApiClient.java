0c2a19b343620a687346c4b65d95a44339fe661f
Askie01 <adam.paw01@gmail.com>
2025-10-30 21:10:30 +0100

    Setup default fallback strategy for `LoansApiClient` inside `accounts` microservice
    * Added a fallback strategy inside `LoansApiClient` that points to `LoansApiClientFallback.class` so we have our fallback strategy enabled.

--- accounts/src/main/java/com/askie01/accounts/client/LoansApiClient.java
+++ accounts/src/main/java/com/askie01/accounts/client/LoansApiClient.java
@@ -1,12 +1,13 @@
 package com.askie01.accounts.client;
 
 import com.askie01.accounts.dto.LoanDTO;
+import com.askie01.accounts.fallback.LoansApiClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "loans", path = "/loans")
+@FeignClient(name = "loans", path = "/loans", fallback = LoansApiClientFallback.class)
 public interface LoansApiClient {
 
     @GetMapping
