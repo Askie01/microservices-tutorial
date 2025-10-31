1f9a6a5a00adcf74640c76d33f25128e3cf49c81
Askie01 <adam.paw01@gmail.com>
2025-10-31 20:43:48 +0100

    Update `Askie01RouteLocatorConfiguration` with retry strategies
    * Added retry strategies for requests coming to `loans` microservice.
    * Now, the retry strategy will only work on GET requests, up to 3 retries with 1s await time (doubled per retry).

--- api-gateway/src/main/java/com/askie01/apigateway/configuration/Askie01RouteLocatorConfiguration.java
+++ api-gateway/src/main/java/com/askie01/apigateway/configuration/Askie01RouteLocatorConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Configuration
@@ -15,6 +17,8 @@ public class Askie01RouteLocatorConfiguration {
         final String segmentReplacement = "/${segment}";
         final String responseTimeHeaderName = "X-Response-Time";
         final String responseTimeHeaderValue = LocalDateTime.now().toString();
+        final Duration firstBackoffDelay = Duration.ofMillis(1000);
+        final Duration maxBackoffDelay = Duration.ofSeconds(1);
         return routeLocatorBuilder.routes()
                 .route(path -> path
                         .path("/askie01/accounts/**")
@@ -35,7 +39,11 @@ public class Askie01RouteLocatorConfiguration {
                         .path("/askie01/loans/**")
                         .filters(filter -> filter
                                 .rewritePath("/askie01/loans/(?<segment>.*)", segmentReplacement)
-                                .addResponseHeader(responseTimeHeaderName, responseTimeHeaderValue))
+                                .addResponseHeader(responseTimeHeaderName, responseTimeHeaderValue)
+                                .retry(config -> config
+                                        .setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(firstBackoffDelay, maxBackoffDelay, 2, true)))
                         .uri("lb://loans"))
                 .build();
     }
