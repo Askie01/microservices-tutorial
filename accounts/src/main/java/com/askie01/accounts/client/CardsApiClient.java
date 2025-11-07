90b640658bd31db3e17ffc914ba4e4d8cc9ef563
Askie01 <adam.paw01@gmail.com>
2025-11-08 00:12:03 +0100

    Remove fallback from `CardsApiClient` in `accounts` microservice
    * Completely removed fallback and any resilience-related configuration in `Cards` feign client.
    * We're removing the resiliency from feign clients, because configuration, documentation and opportunities are dogshit.

--- accounts/src/main/java/com/askie01/accounts/client/CardsApiClient.java
+++ accounts/src/main/java/com/askie01/accounts/client/CardsApiClient.java
@@ -1,13 +1,12 @@
 package com.askie01.accounts.client;
 
 import com.askie01.accounts.dto.CardDTO;
-import com.askie01.accounts.fallback.CardsApiClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "cards", path = "/cards", fallback = CardsApiClientFallback.class)
+@FeignClient(name = "cards", path = "/cards")
 public interface CardsApiClient {
 
     @GetMapping
