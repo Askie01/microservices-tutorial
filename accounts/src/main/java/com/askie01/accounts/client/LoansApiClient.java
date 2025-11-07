e19152a13a0d6f42b1cd1f4c217608846747be8e
Askie01 <adam.paw01@gmail.com>
2025-11-08 00:12:25 +0100

    Remove fallback from `LoansApiClient` in `accounts` microservice
    * Completely removed fallback and any resilience-related configuration in `Loans` feign client.
    * We're removing the resiliency from feign clients, because configuration, documentation and opportunities are dogshit.

--- accounts/src/main/java/com/askie01/accounts/client/LoansApiClient.java
+++ accounts/src/main/java/com/askie01/accounts/client/LoansApiClient.java
@@ -1,13 +1,12 @@
 package com.askie01.accounts.client;
 
 import com.askie01.accounts.dto.LoanDTO;
-import com.askie01.accounts.fallback.LoansApiClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "loans", path = "/loans", fallback = LoansApiClientFallback.class)
+@FeignClient(name = "loans", path = "/loans")
 public interface LoansApiClient {
 
     @GetMapping
