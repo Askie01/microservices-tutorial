a36a795ca87cc61abb7755594250845c75e2b68a
Askie01 <adam.paw01@gmail.com>
2025-11-07 23:56:35 +0100

    Create `LoanApiResilienceService` in `accounts` microservice
    * Created a resilience loan API service that calls external `loans` microservice.
    * This service is taking care of fault-tolerant communication with `loans` microservice.

---
+++ accounts/src/main/java/com/askie01/accounts/service/LoanApiResilienceService.java
@@ -0,0 +1,26 @@
+package com.askie01.accounts.service;
+
+import com.askie01.accounts.client.LoansApiClient;
+import com.askie01.accounts.dto.LoanDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class LoanApiResilienceService implements LoanResilienceService {
+
+    private final LoansApiClient loansApiClient;
+
+    @CircuitBreaker(name = "loans-api-client-circuit-breaker")
+    @TimeLimiter(name = "loans-api-client-time-limiter")
+    @Retry(name = "loans-api-client-retry")
+    public CompletableFuture<LoanDTO> getLoan(String mobileNumber) {
+        return CompletableFuture.supplyAsync(() ->
+                loansApiClient.getLoan(mobileNumber).getBody());
+    }
+}
