c1e9be54bf1ce5e76b5cdf9d2f56ec4506a41157
Askie01 <adam.paw01@gmail.com>
2025-11-22 18:49:05 +0100

    Add receive & complete request logs in `CustomerBankDetailsController`
    * Added logs for a request in `CustomerBankDetailsController` - one when the request is received, and one when the request is completed.

--- accounts/src/main/java/com/askie01/accounts/controller/CustomerBankDetailsController.java
+++ accounts/src/main/java/com/askie01/accounts/controller/CustomerBankDetailsController.java
@@ -4,6 +4,7 @@ import com.askie01.accounts.dto.CustomerBankDetailsDTO;
 import com.askie01.accounts.service.CustomerBankDetailsService;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/customers")
 @RequiredArgsConstructor
@@ -21,7 +23,9 @@ public class CustomerBankDetailsController {
     @GetMapping
     public ResponseEntity<CustomerBankDetailsDTO> getCustomerBankDetails(@Pattern(regexp = "(^$|\\d{9})", message = "Mobile number must be 9 digits")
                                                                          @RequestParam String mobileNumber) {
+        log.atInfo().log("Received `getCustomerBankDetails` GET request");
         final CustomerBankDetailsDTO customerBankDetails = customerBankDetailsService.getCustomerBankDetails(mobileNumber);
+        log.atInfo().log("Completed `getCustomerBankDetails` GET request");
         return new ResponseEntity<>(customerBankDetails, HttpStatus.OK);
     }
 }
